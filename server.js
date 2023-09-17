ef8397183bf3af2b0b422554eb1b75d3c6f31ab6
Staroyee <danmasefield@gmail.com>
2023-09-17 14:11:59 +0930

    Created user routes

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const helpers = require('./utils/helpers');
+const path = require('path');
+//const helpers = require('./utils/helpers');
+const hbs = exphbs.create({});
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-const hbs = exphbs.create({ helpers });
 
 const sess = {
     secret: 'Super secret secret',
@@ -42,4 +42,3 @@ app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
