538d16eec410507dbca700cfbe1cd2f8e975524f
luanillustrates <luanillustrates@gmail.com>
2023-09-19 18:54:08 +0930

    ready to merge into main

--- server.js
+++ server.js
@@ -1,9 +1,5 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-<<<<<<< HEAD
-// const routes = require('./controllers');
-// const helpers = require('./utils/helpers');
-=======
 const session = require('express-session');
 const routes = require('./controllers');
 const path = require('path');
@@ -11,7 +7,6 @@ const path = require('path');
 const hbs = exphbs.create({});
 var morgan = require('morgan');
 var helmet = require('helmet');
->>>>>>> f7c5ef0b472727169c3822f8276a1d3c6aef0644
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -19,35 +14,6 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-<<<<<<< HEAD
-// const hbs = exphbs.create({ helpers });
-
-const sess = {
-    secret: 'Super secret secret',
-    cookie: {
-      maxAge: 300000,
-      httpOnly: true,
-      secure: false,
-      sameSite: 'strict',
-    },
-    resave: false,
-    saveUninitialized: true,
-    // store: new SequelizeStore({
-    //   db: sequelize
-    // })
-  };
-
-app.use(session(sess));
-
-// app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use(routes);
-=======
 app.use(morgan('combined'));
 app.use(helmet());
 app.engine('handlebars', hbs.engine);
@@ -68,7 +34,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
->>>>>>> f7c5ef0b472727169c3822f8276a1d3c6aef0644
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
