27bca2cf7425d4187d786c26a5f917c59d52c89a
Staroyee <danmasefield@gmail.com>
2023-09-20 11:09:03 +0930

    created routes to search for products

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,163 +1,208 @@
 const router = require('express').Router();
+const { Op } = require('sequelize');
 const withAuth = require('../utils/withAuth');
-const { Product, Category, Tag, ProductTag, User, Order } = require('../models');
+const {
+  Product,
+  Category,
+  Tag,
+  ProductTag,
+  User,
+  Order,
+} = require('../models');
 
 router.get('/', async (req, res) => {
-    try {
-        // Get all product and JOIN with category data
-        const productData = await Product.findAll({
-            include: [
-                {
-                    model: Category,
-                    attributes: ['category_name'],
-                },
-            ],
-        });
+  try {
+    // Get all product and JOIN with category data
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+      ],
+    });
 
-        // // Testing for insomnia
-        // res.json(productData);
+    // // Testing for insomnia
+    // res.json(productData);
 
-        // TODO: Uncomment after view page is complete
-        // Serialize data so the template can read it
-        const products = productData.map((product) => product.get({ plain: true }));
+    // TODO: Uncomment after view page is complete
+    // Serialize data so the template can read it
+    const products = productData.map((product) => product.get({ plain: true }));
 
+    // // Pass serialized data and session flag into template
+    res.render('homepage', {
+      products,
+    });
+  } catch (err) {
+    // Render an error page with a user-friendly message
+    res.render('error', { error: 'An error occurred while fetching data.' });
+  }
+});
 
-        // // Pass serialized data and session flag into template
-        res.render('homepage', {
-            products
-        });
+// const searchTerm = 'search term'; // Replace with the user's input
 
-    } catch (err) {
-        // Render an error page with a user-friendly message
-        res.render('error', { error: 'An error occurred while fetching data.' });
-    }
+// Product.findAll({
+//   where: {
+//     productName: {
+//       [Op.iLike]: `%${searchTerm}%`,
+//     },
+//   },
+// })
+//   .then((products) => {
+//     // Process the products that match the search term
+//     console.log(products);
+//   })
+//   .catch((error) => {
+//     console.error('Error searching for products:', error);
+//   });
+
+router.get('/search', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      where: {
+        product_name: {
+          [Op.like]: `%${req.body.searchTerm}%`,
+        },
+      },
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+      ],
+    });
+    const products = productData.map((product) => product.get({ plain: true }));
+    console.log(products);
+    console.log('error');
+    res.render('allProducts', {
+      products,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/products', async (req, res) => {
-    try {
-        const productData = await Product.findAll({
-            include: [
-                {
-                    model: Category,
-                    attributes: ['category_name'],
-                },
-            ],
-        });
-        console.log(productData);
-        const products = productData.get({ plain: true });
-
-        res.render('products', {
-            ...products
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const productData = await Product.findAll({
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+      ],
+    });
+    const products = productData.map((product) => product.get({ plain: true }));
+    console.log(products);
+    console.log('error');
+    res.render('allProducts', {
+      products,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // Product view :show product by id
 router.get('/products/:id', async (req, res) => {
-    try {
-        const productData = await Product.findByPk(req.params.id, {
-            include: [
-                {
-                    model: Category,
-                    attributes: ['category_name'],
-                },
-            ],
-        });
-        const product = productData.get({ plain: true });
-        console.log(product);
-        res.render('products', {
-            ...product
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        {
+          model: Category,
+          attributes: ['category_name'],
+        },
+      ],
+    });
+    const product = productData.get({ plain: true });
+    console.log(product);
+    res.render('products', {
+      ...product,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 
 // Category view ;show category by id
 router.get('/accessories', async (req, res) => {
-    try {
-        const categoryData = await Category.findByPk(3, {
-            include: [{ model: Product }]
-        });
+  try {
+    const categoryData = await Category.findByPk(3, {
+      include: [{ model: Product }],
+    });
 
-        const category = categoryData.get({ plain: true });
-        res.render('singleCategory', {
-            ...category
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    const category = categoryData.get({ plain: true });
+    res.render('singleCategory', {
+      ...category,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/equipment', async (req, res) => {
-    try {
-        const categoryData = await Category.findByPk(2, {
-            include: [{ model: Product }]
-        });
+  try {
+    const categoryData = await Category.findByPk(2, {
+      include: [{ model: Product }],
+    });
 
-        const category = categoryData.get({ plain: true });
-        res.render('singleCategory', {
-            ...category
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    const category = categoryData.get({ plain: true });
+    res.render('singleCategory', {
+      ...category,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/supplements', async (req, res) => {
-    try {
-        const categoryData = await Category.findByPk(1, {
-            include: [{ model: Product }]
-        });
+  try {
+    const categoryData = await Category.findByPk(1, {
+      include: [{ model: Product }],
+    });
 
-        const category = categoryData.get({ plain: true });
-        res.render('singleCategory', {
-            ...category
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+    const category = categoryData.get({ plain: true });
+    res.render('singleCategory', {
+      ...category,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
 
 // WithAuth middleware to prevent access to account view
 router.get('/account', withAuth, async (req, res) => {
-    try {
-        // Find the logged in user based on the session ID
-        const userData = await User.findByPk(req.session.user_id, {
-            include: [{ model: Order }],
-        });
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      include: [{ model: Order }],
+    });
 
-        const user = userData.get({ plain: true });
+    const user = userData.get({ plain: true });
 
-        // Check if session expired
-        if (req.session.loggedIn) {
-            res.render('account', {
-                // User details & cart
-                ...user,
-            });
-        }
-
-        res.redirect('/');
-        return;
-    } catch (err) {
-        res.status(500).json(err);
+    // Check if session expired
+    if (req.session.loggedIn) {
+      res.render('account', {
+        // User details & cart
+        ...user,
+      });
     }
-});
 
+    res.redirect('/');
+    return;
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.get('/login', (req, res) => {
-    // If the user is already logged in, redirect the request to another route
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
-    res.render('login');
+  res.render('login');
 });
-
 
 module.exports = router;
