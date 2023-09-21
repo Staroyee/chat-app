a62508ad6eb21473cd9aebe2b69dbfc16cbdf2ef
Staroyee <danmasefield@gmail.com>
2023-09-21 11:53:35 +0930

    css

--- public/js/login.js
+++ public/js/login.js
@@ -44,6 +44,20 @@ const loginFormHandler = async (event) => {
       }
     }
   };
+
+  async function logout() {
+    const res = await fetch('/api/users/logout', {
+        method: 'post',
+        headers: { 'Content-Type': 'application/json' }
+    });
+    if (res.ok) {
+        document.location.replace('/');
+    } else {
+        alert(res.statusText);
+    }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
   
   document
     .querySelector('.login-form')
