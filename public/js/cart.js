94cc8c9a97f8f3a026423fc5e3f1450a2222275a
eliabn <30064396@tafe.wa.edu.au>
2023-09-25 00:20:26 +0930

    Started Remove from cart function

--- public/js/cart.js
+++ public/js/cart.js
@@ -16,3 +16,23 @@ viewCartButton.addEventListener("click", function () {
   window.location.href = `/cart?productIds=${JSON.stringify(cartProducts)}`;
 });
 
+
+
+const removeFromCartButtons = document.querySelectorAll(".addToCart");
+
+//const viewCartButton = document.getElementById("viewCart");
+
+//const cartProducts = JSON.parse(localStorage.getItem("cartProduct")) || [];
+
+addToCartButtons.forEach(button => {
+  button.addEventListener("click", function () {
+    const productId = button.getAttribute("data-product-id");
+    cartProducts.push(productId);
+    localStorage.removeItem("cartProduct", JSON.stringify(cartProducts));
+  });
+});
+
+viewCartButton.addEventListener("click", function () {
+  window.location.href = `/cart?productIds=${JSON.stringify(cartProducts)}`;
+});
+
