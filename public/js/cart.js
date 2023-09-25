39451a52efcd8c3c6c50a70611dac2127b9aac43
eliabn <30064396@tafe.wa.edu.au>
2023-09-25 12:56:03 +0930

    Remove items from cart.js

--- public/js/cart.js
+++ public/js/cart.js
@@ -1,38 +1,58 @@
-const addToCartButtons = document.querySelectorAll(".addToCart");
+if (typeof document !== 'undefined') {
+    const addToCartButtons = document.querySelectorAll(".addToCart");
 
-const viewCartButton = document.getElementById("viewCart");
+    const viewCartButton = document.getElementById("viewCart");
 
-const cartProducts = JSON.parse(localStorage.getItem("cartProduct")) || [];
+    const cartProducts = JSON.parse(localStorage.getItem("cartProduct")) || [];
 
-addToCartButtons.forEach(button => {
-  button.addEventListener("click", function () {
-    const productId = button.getAttribute("data-product-id");
-    cartProducts.push(productId);
-    localStorage.setItem("cartProduct", JSON.stringify(cartProducts));
-  });
-});
+    addToCartButtons.forEach(button => {
+        button.addEventListener("click", function () {
+            const productId = button.getAttribute("data-product-id");
+            cartProducts.push(productId);
+            localStorage.setItem("cartProduct", JSON.stringify(cartProducts));
+        });
+    });
 
-viewCartButton.addEventListener("click", function () {
-  window.location.href = `/cart?productIds=${JSON.stringify(cartProducts)}`;
-});
+    viewCartButton.addEventListener("click", function () {
+        window.location.href = `/cart?productIds=${JSON.stringify(cartProducts)}`;
+    });
 
 
 
-const removeFromCartButtons = document.querySelectorAll(".addToCart");
+    // const removeFromCartButtons = document.querySelectorAll(".removeFromCart");
 
-//const viewCartButton = document.getElementById("viewCart");
+    // //const viewCartButton = document.getElementById("viewCart");
 
-//const cartProducts = JSON.parse(localStorage.getItem("cartProduct")) || [];
+    // //const cartProducts = JSON.parse(localStorage.getItem("cartProduct")) || [];
 
-addToCartButtons.forEach(button => {
-  button.addEventListener("click", function () {
-    const productId = button.getAttribute("data-product-id");
-    cartProducts.push(productId);
-    localStorage.removeItem("cartProduct", JSON.stringify(cartProducts));
-  });
-});
+    // removeFromCartButtons.addEventListener("click", function () {
+    //     console.log('Yooo')
+    //     const productId = button.getAttribute("data-product-id");
+    //     cartProducts.push(productId);
+    //     var filtered = someArray.filter(function (el) { return el.Name != "Kristian"; });
+    //     localStorage.removeItem("cartProduct", JSON.stringify(cartProducts));
+    // });
 
-viewCartButton.addEventListener("click", function () {
-  window.location.href = `/cart?productIds=${JSON.stringify(cartProducts)}`;
-});
 
+    // Remove from Cart
+    const removeButtons = document.querySelectorAll('.remove-button');
+
+    async function removeFromCart(productId) {
+
+        const updatedCart = cartProducts.filter(function (toRemove) {
+            return toRemove !== productId; // Remove items matching the productId
+        });
+
+        localStorage.setItem("cartProduct", JSON.stringify(updatedCart)); // Update the entire "cartProduct" key
+
+        cartProducts = updatedCart; // Update the cartProducts variable
+    }
+
+    removeButtons.forEach((button) => {
+        button.addEventListener('click', function () {
+            const productId = this.id;
+            removeFromCart(productId);
+        });
+    });
+
+}
