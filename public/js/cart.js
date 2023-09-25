0f762889f2975c80b54ff6c43b63c6700a275ee8
Staroyee <danmasefield@gmail.com>
2023-09-25 12:31:45 +0930

    Created delete buttons for cart items

--- public/js/cart.js
+++ public/js/cart.js
@@ -32,5 +32,38 @@ addToCartButtons.forEach(addToCartButtons => {
   })
 });
 
+function deleteProduct(productId) {
+  const index = cartProducts.indexOf(productId);
+  if (index !== -1) {
+    cartProducts.splice(index, 1);
+    localStorage.setItem("cartProduct", JSON.stringify(cartProducts));
+  }
+
+  // Remove the deleted item from the query string
+  const urlParams = new URLSearchParams(window.location.search);
+  urlParams.delete('productIds');
+  if (cartProducts.length > 0) {
+    urlParams.set('productIds', JSON.stringify(cartProducts));
+  }
+
+  // Update the URL without refreshing the page
+  window.history.replaceState({}, document.title, `?${urlParams.toString()}`);
+}
+
+// Add a click event listener to all delete buttons
+document.querySelectorAll(".delete-button").forEach(deleteButton => {
+  deleteButton.addEventListener("click", function () {
+    const productId = deleteButton.getAttribute("data-product-id");
+    deleteProduct(productId);
+    
+    // Remove the deleted item from the DOM
+    deleteButton.closest("li").remove();
+  });
+});
+
+
+
+
+
 
 
